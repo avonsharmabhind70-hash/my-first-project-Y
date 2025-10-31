cfcf22085e8501c5d194702da15da4f9ac813b89
avonsharmabhind70-hash <avonsharmabhind70@gmail.com>
2025-11-01 03:45:14 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end('File not found');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    }
+});
+
+server.listen(5500, '127.0.0.1', () => {
+    console.log('Server running at http://127.0.0.1:5500/');
+});
